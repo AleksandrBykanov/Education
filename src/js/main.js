8f2a970466c6ff5755e57ae0dd5908773b70c408
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-03-05 16:31:57 +0200

    speakers

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 import '../scss/style.scss'
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sliders = document.querySelectorAll(".slider");
+
+  sliders.forEach(slider => {
+      const buttons = slider.querySelectorAll(".slider-btn");
+      const slides = slider.querySelectorAll(".slide");
+
+      buttons.forEach((button, index) => {
+          button.addEventListener("click", () => {
+              slides.forEach(slide => slide.classList.remove("active"));
+              buttons.forEach(btn => btn.classList.remove("active"));
+              slides[index].classList.add("active");
+              button.classList.add("active");
+          });
+      });
+  });
+});
+
+
+  
