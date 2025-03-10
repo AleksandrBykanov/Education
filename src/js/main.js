9c2b74ec764d39e1f7c1bced8bc58509ef86ca4c
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-03-10 11:58:31 +0200

    fix modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 import "../scss/style.scss";
 
+const burgerInput = document.querySelector('.burger-input');
+const modal = document.querySelector('.modal');
+
+
+burgerInput.addEventListener('change', () => {
+  if (burgerInput.checked) {
+    modal.classList.add('active');
+  } else {
+    modal.classList.remove('active');
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const sliders = document.querySelectorAll('.speakers-container');
 
